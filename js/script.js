1eb2f710a94f62a9bb5e936d65349ef1ac9c427e
Tim <gleb285@gmail.com>
2020-07-30 17:27:55 +0300

    js first app

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
-"use strict";
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
 
-const category = 'toys';
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-console.log(`https://someurl.com/${category}/5`)
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?', '');
 
-const user = 'Ivan';
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-alert(`Привет, ${user}`);
+console.log(personalMovieDB);
