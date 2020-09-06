f9f9eb1d7fe2e56fd7ba1136af8022411ae5574f
Tim <gleb285@gmail.com>
2020-09-06 13:37:48 +0200

    status

--- script.js
+++ script.js
@@ -17,19 +17,38 @@ if (1) {
 
 // (num === 50) ? console.log('Ok!'): console.log('Error!');
 
-const num = 50;
+let num = 50;
 
-switch (num) {
-    case 49:
-        console.log('Неверное');
-        break;
-    case 100:
-        console.log('НЕА');
-        break;
-    case 50:
-        console.log('В точку');
-        break;
-    default:
-        console.log('Не в этот раз');
-        break;
+// while (num < 55){
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+
+for (let i = 1; i < 10; i++) {
+    if (i == 6) {
+        // break;
+        continue;
+    }
+    console.log(i)
 }
+
+// switch (num) {
+//     case 49:
+//         console.log('Неверное');
+//         break;
+//     case 100:
+//         console.log('НЕА');
+//         break;
+//     case 50:
+//         console.log('В точку');
+//         break;
+//     default:
+//         console.log('Не в этот раз');
+//         break;
+// }
